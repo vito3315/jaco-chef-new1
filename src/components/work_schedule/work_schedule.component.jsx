0a04a274673c0b9f62e545f7bf3c413868df5ca0
vito3315 <marusiaqwerti@yandex.ru>
2021-11-28 20:30:07 +0400

    test

--- src/components/work_schedule/work_schedule.component.jsx
+++ src/components/work_schedule/work_schedule.component.jsx
@@ -34,6 +34,22 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import CloseIcon from '@mui/icons-material/Close';
 import AddIcon from '@mui/icons-material/Add';
 
+import PropTypes from 'prop-types';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import PersonIcon from '@mui/icons-material/Person';
+import { blue } from '@mui/material/colors';
+
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import SyncAltIcon from '@mui/icons-material/SyncAlt';
+
+import LooksOneIcon from '@mui/icons-material/LooksOne';
+import LooksTwoIcon from '@mui/icons-material/LooksTwo';
+import Looks3Icon from '@mui/icons-material/Looks3';
+
 import { MySelect, MyCheckBox, MyTimePicker } from '../../stores/elements';
 
 const queryString = require('query-string');
@@ -51,6 +67,58 @@ const useStyles = makeStyles({
     width: '100%'
   }
 });
+
+function SimpleDialog(props) {
+  const { onClose, selectedValue, open } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = (value) => {
+    onClose(value);
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <List sx={{ pt: 0 }}>
+        
+        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <AccessTimeIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Сменить часы на месяц" />
+        </ListItem>
+        
+        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <SyncAltIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Сменить смену" />
+        </ListItem>
+        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <HomeWorkIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Сменить точку" />
+        </ListItem>
+        
+      </List>
+    </Dialog>
+  );
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+};
 
 class WorkSchedule_ extends React.Component {
   constructor(props) {
@@ -81,6 +149,19 @@ class WorkSchedule_ extends React.Component {
       
       newTimeStart: '10:00',
       newTimeEnd: '22:00',
+      
+      openNewTimeAdd: false,
+      
+      otherAppList: [],
+      otherApp: '',
+      
+      testVal: '',
+      testOpen: false,
+      
+      mainMenu: false,
+      mainMenuH: false,
+      
+      chooseUser: null
     };
   }
   
@@ -166,6 +247,11 @@ class WorkSchedule_ extends React.Component {
       return json;
     })
     .catch(err => { 
+      setTimeout( () => {
+        this.setState({
+          is_load: false
+        })
+      }, 300 )
       console.log( err )
     });
   }
@@ -213,6 +299,7 @@ class WorkSchedule_ extends React.Component {
     this.setState({
       isOpenModalH: true,
       userInfo: res.h_info,
+      otherAppList: res.other_app
     })
   }
   
@@ -226,12 +313,161 @@ class WorkSchedule_ extends React.Component {
     })
   }
   
+  changeHourse(type, key, event){
+    let userInfo = this.state.userInfo;
+    
+    userInfo.hours[ key ][ [type] ] = event.target.value;
+    
+    this.setState({
+      userInfo: userInfo
+    })
+  }
+  
+  async saveDayHourse(){
+    console.log( this.state.userInfo )
+    
+    let data = {
+      date: this.state.userInfo.date,
+      user_id: this.state.userInfo.user_id,
+      smena_id: this.state.userInfo.smena_id,
+      app_id: this.state.userInfo.app_id,
+      hours: this.state.userInfo.hours,
+      new_app: this.state.userInfo.new_app,
+      mentor_id: this.state.userInfo.mentor_id
+    }
+    
+    let res = await this.getData('save_user_day', data);
+    
+    console.log( res )
+  }
+  
+  addTime(){
+    let userInfo = this.state.userInfo;
+    
+    let check = userInfo.hours.find( (item) => item.time_start == this.state.newTimeStart && item.time_end == this.state.newTimeEnd );
+    
+    if( check ){
+      this.setState({
+        openNewTimeAdd: false
+      })
+      
+      return ;
+    }
+    
+    userInfo.hours.push({
+      time_start: this.state.newTimeStart,
+      time_end: this.state.newTimeEnd
+    })
+    
+    this.setState({
+      userInfo: userInfo,
+      openNewTimeAdd: false
+    })
+  }
+  
+  async fastTime(type){
+    let data = {
+      type: type,
+      user_id: this.state.chooseUser.id,
+      app_id: this.state.chooseUser.app_id,
+      smena_id: this.state.chooseUser.id,
+      date: this.state.mounth
+    }
+    
+    let res = await this.getData('save_fastTime', data);
+    
+    console.log( res );
+  }
+  
   render(){
     return (
       <>
         <Backdrop open={this.state.is_load}>
           <CircularProgress color="inherit" />
         </Backdrop>
+        
+        <Dialog onClose={ () => { this.setState({ mainMenu: false }) } } open={this.state.mainMenu}>
+          
+          { !this.state.chooseUser ? null :
+            <DialogTitle>{this.state.chooseUser.full_app_name} {this.state.chooseUser.user_name} {this.state.mounth}</DialogTitle>
+          }
+          
+          <List sx={{ pt: 0 }}>
+            
+            <ListItem button onClick={ () => { this.setState({ mainMenu: false, mainMenuH: true }) } }>
+              <ListItemAvatar>
+                <Avatar>
+                  <AccessTimeIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Сменить часы на месяц" />
+            </ListItem>
+            
+            <ListItem button>
+              <ListItemAvatar>
+                <Avatar>
+                  <SyncAltIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Сменить смену" />
+            </ListItem>
+            <ListItem button>
+              <ListItemAvatar>
+                <Avatar>
+                  <HomeWorkIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Сменить точку" />
+            </ListItem>
+            
+          </List>
+        </Dialog>
+        
+        <Dialog onClose={ () => { this.setState({ mainMenuH: false }) } } open={this.state.mainMenuH}>
+          
+          { !this.state.chooseUser ? null :
+            <DialogTitle>{this.state.chooseUser.user_name} {this.state.mounth} Часы</DialogTitle>
+          }
+          
+          <List sx={{ pt: 0 }}>
+            
+            <ListItem button onClick={this.fastTime.bind(this, 1)}>
+              <ListItemAvatar>
+                <Avatar>
+                  <LooksOneIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="С 1 числа 2/2 с 10 до 22 на месяц" />
+            </ListItem>
+            
+            <ListItem button onClick={this.fastTime.bind(this, 2)}>
+              <ListItemAvatar>
+                <Avatar>
+                  <LooksTwoIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Со 2 числа 2/2 с 10 до 22 на месяц" />
+            </ListItem>
+            <ListItem button onClick={this.fastTime.bind(this, 3)}>
+              <ListItemAvatar>
+                <Avatar>
+                  <Looks3Icon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="С 3 числа 2/2 с 10 до 22 на месяц" />
+            </ListItem>
+            <ListItem button onClick={this.fastTime.bind(this, 4)}>
+              <ListItemAvatar>
+                <Avatar>
+                  <Looks3Icon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="С 4 числа 2/2 с 10 до 22 на месяц" />
+            </ListItem>
+            
+          </List>
+        </Dialog>
+        
         
         { !this.state.userInfo ? null :
           <Dialog
@@ -245,9 +481,21 @@ class WorkSchedule_ extends React.Component {
             <DialogContent dividers={true}>
               
               <Typography>{'Моя нагрузка: ' + this.state.userInfo.user.my_load_h + ' / Средняя нагрузка: ' + this.state.userInfo.user.all_load_h}</Typography>
-              <Typography>{'Бонус: ' + this.state.userInfo.user.bonus}</Typography>
+              <Typography style={{ marginBottom: 10 }}>{'Бонус: ' + this.state.userInfo.user.bonus}</Typography>
               
-              <Accordion>
+              { this.state.otherAppList.length == 0 ? null :
+                <MySelect data={this.state.otherAppList} value={this.state.userInfo.new_app} func={ (event) => { let userInfo = this.state.userInfo; userInfo.new_app = event.target.value; this.setState({ userInfo: userInfo }) } } label='Кем работает' />
+              }
+              
+              { this.state.userInfo.mentor_list.length == 0 ? null :
+                <MySelect data={this.state.userInfo.mentor_list} value={this.state.userInfo.mentor_id} func={ (event) => { let userInfo = this.state.userInfo; userInfo.mentor_id = event.target.value; this.setState({ userInfo: userInfo }) } } label='Кем работает' />
+              }
+              
+              <Accordion 
+                style={{ marginTop: 10 }} 
+                expanded={this.state.openNewTimeAdd} 
+                onChange={ () => { this.setState({ openNewTimeAdd: !this.state.openNewTimeAdd }) } }>
+                  
                 <AccordionSummary
                   expandIcon={<AddIcon />}
                 >
@@ -258,6 +506,8 @@ class WorkSchedule_ extends React.Component {
                   
                   <MyTimePicker value={this.state.newTimeStart} func={ (event) => { this.setState({ newTimeStart: event.target.value }) } } classes={this.state.classes} label='Время начала работы' />
                   <MyTimePicker value={this.state.newTimeEnd} func={ (event) => { this.setState({ newTimeEnd: event.target.value }) } } classes={this.state.classes} label='Время окончания работы' />
+                  
+                  <AddIcon style={{ minWidth: 50, minHeight: 38, cursor: 'pointer' }} onClick={ this.addTime.bind(this) } />
                   
                 </AccordionDetails>
               </Accordion>  
@@ -270,20 +520,39 @@ class WorkSchedule_ extends React.Component {
                     <AccessTimeIcon style={{ marginRight: 10 }} />
                     <Typography>{item.time_start + ' - ' + item.time_end}</Typography>
                   </AccordionSummary>
-                  <AccordionDetails>
-                    <Typography>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                      malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
+                  <AccordionDetails style={{ display: 'flex', flexDirection: 'row' }}>
+                    <MyTimePicker value={item.time_start} func={ this.changeHourse.bind(this, 'time_start', key) } classes={this.state.classes} label='Время начала работы' />
+                    <MyTimePicker value={item.time_end} func={ this.changeHourse.bind(this, 'time_end', key) } classes={this.state.classes} label='Время окончания работы' />
                   </AccordionDetails>
                 </Accordion>  
               ) }
               
               
+              <Accordion style={{ marginTop: 50 }} disabled>
+                <AccordionSummary>
+                  <Typography>История</Typography>
+                </AccordionSummary>
+              </Accordion>  
+              
+              { this.state.userInfo.hist.map( (item, key) =>
+                <Accordion key={key}>
+                  <AccordionSummary>
+                    <Typography>{item.date + ' - ' + item.user_name}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
+                    
+                    { item.items.map( (it, k) =>
+                      <Typography key={k}>{it.time_start + ' - ' + it.time_end} {it.app_name == '' ? '' : ' - ' + it.app_name} </Typography>
+                    ) }
+                    
+                  </AccordionDetails>
+                </Accordion>  
+              ) }
+              
             </DialogContent>
-            <DialogActions>
-              <Button onClick={() => { this.setState({ isOpenModalH: false }) }}>Cancel</Button>
-              <Button onClick={() => { this.setState({ isOpenModalH: false }) }}>Subscribe</Button>
+            <DialogActions style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Button style={{ backgroundColor: 'green', color: '#fff' }} onClick={this.saveDayHourse.bind(this)}>Сохранить</Button>
+              <Button style={{ backgroundColor: 'red', color: '#fff' }} onClick={() => { this.setState({ isOpenModalH: false }) }}>Отмена</Button>
             </DialogActions>
           </Dialog>
         }
@@ -346,7 +615,7 @@ class WorkSchedule_ extends React.Component {
                         <TableRow key={key}>
                           <TableCell>{item.data.user_name}</TableCell>
                           <TableCell>{item.data.app_name}</TableCell>
-                          <TableCell></TableCell>
+                          <TableCell style={{ textAlign: 'center' }}> <SyncAltIcon style={{ cursor: 'pointer' }} onClick={ () => { this.setState({ mainMenu: true, chooseUser: item.data }) } } /> </TableCell>
                           
                           { item.data.dates.map( (date, date_k) =>
                             <TableCell onClick={ this.openH.bind(this, item.data, date.date) } className="min_block" style={{ backgroundColor: date.info ? date.info.color : '#fff' }} key={date_k}>{date.info ? date.info.hours : ''}</TableCell>
