693f36a6b0502c125b18a44668daf0a8bc713ff3
vito3315 <marusiaqwerti@yandex.ru>
2022-02-24 20:18:13 +0400

    test

--- src/components/app_work_show/app_work_show.component.jsx
+++ src/components/app_work_show/app_work_show.component.jsx
@@ -26,6 +26,13 @@ import ListItemText from '@mui/material/ListItemText';
 
 import CloseIcon from '@mui/icons-material/Close';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
+
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
 import Typography from '@mui/material/Typography';
 
@@ -81,6 +88,37 @@ const useStyles = makeStyles({
   }
 });
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>{children}</Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 class AppWorkShow_ extends React.Component {
   constructor(props) {
     super(props);
@@ -94,8 +132,11 @@ class AppWorkShow_ extends React.Component {
       
       items: [],
       items_min: [],
+      showItems: [],
       modalDialog: false,
       modalDialogNew: false,
+
+      activeTab: 0,
 
       dows: [
         {id: 1, name: 'Понедельник'},
@@ -123,24 +164,24 @@ class AppWorkShow_ extends React.Component {
 
       itemsNew: null,
       chengeItem1: null,
-      chengeItemNew1: null
+      chengeItemNew1: null,
+
+      points: [],
+      point_id: ''
     };
   }
   
   async componentDidMount(){
-    let data = await this.getData('get_all');
+    let points = await this.getData('get_points');
     
-    data.items.map( (item, key) => {
-      data.items[key]['dow_name'] = this.state.dows.find( (it) => parseInt(it.id) == parseInt(item.dow) )['name'];
-    } )
-
     this.setState({
-      module_name: data.module_info.name,
-      items: data.items,
-      items_min: data.items_min
+      points: points,
+      point_id: points[0]['id']
     })
-    
-    document.title = data.module_info.name;
+
+    setTimeout( () => {
+      this.getWorks();
+    }, 300 )
   }
   
   getData = (method, data = {}) => {
@@ -188,9 +229,43 @@ class AppWorkShow_ extends React.Component {
     });
   }
    
+  async getWorks(){
+    let data = {
+      point_id: this.state.point_id
+    };
+
+    let res = await this.getData('get_all', data);
+
+    res.items.map( (item, key) => {
+      res.items[key]['dow_name'] = this.state.dows.find( (it) => parseInt(it.id) == parseInt(item.dow) )['name'];
+    } )
+
+    let activeTab = parseInt(this.state.activeTab);
+
+    if( activeTab == 0 ){
+      activeTab = 5;
+    }
+    if( activeTab == 1 ){
+      activeTab = 6;
+    }
+    if( activeTab == 2 ){
+      activeTab = 21;
+    }
+
+    this.setState({
+      module_name: res.module_info.name,
+      items: res.items,
+      showItems: res.items.filter( (item) => parseInt(item.app_id) == parseInt(activeTab) ),
+      items_min: res.items_min
+    })
+
+    document.title = res.module_info.name;
+  }
+
   async openWork(id){
     let data = {
-      id: id
+      id: id,
+      point_id: this.state.point_id
     };
 
     let res = await this.getData('get_one', data);
@@ -203,6 +278,25 @@ class AppWorkShow_ extends React.Component {
     })
   }
 
+  changeTab(event, val){
+    let activeTab = parseInt(val);
+
+    if( val == 0 ){
+      activeTab = 5;
+    }
+    if( val == 1 ){
+      activeTab = 6;
+    }
+    if( val == 2 ){
+      activeTab = 21;
+    }
+
+    this.setState({
+      activeTab: val,
+      showItems: this.state.items.filter( (item) => parseInt(item.app_id) == parseInt(activeTab) )
+    })
+  }
+
   render(){
     return (
       <>
@@ -210,40 +304,60 @@ class AppWorkShow_ extends React.Component {
           <CircularProgress color="inherit" />
         </Backdrop>
         
-        { !this.state.itemsEdit ? null :
-          <Dialog
-            open={this.state.modalDialog}
-            maxWidth={'md'}
-            onClose={ () => { this.setState({ modalDialog: false, itemsEdit: null, nameWork: '' }) } }
-          >
-            <DialogTitle>Уборка "{this.state.itemsEdit.item.name}"</DialogTitle>
-            <DialogContent>
-              
-              <Grid container spacing={0}>
-
-                { this.state.itemsEdit.item.show_time == false ? null :
-                  <Grid item xs={12}>
-                    <Typography>Время за уборку: {this.state.itemsEdit.itemюtime_min} мин.</Typography>
-                  </Grid>
-                }
-
-                <Grid item xs={12}>
-                  <Typography>Описание процесса уборки: </Typography>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <Typography dangerouslySetInnerHTML={{__html: this.state.itemsEdit.item.text_work}}></Typography>
-                </Grid>
-                
-              </Grid>
-
-            </DialogContent>
-          </Dialog>
-        }
+        
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={12}>
             <h1>{this.state.module_name}</h1>
+          </Grid>
+
+          <Grid item xs={12} sm={3}>
+            <MySelect classes={this.state.classes} data={this.state.points} value={this.state.point_id} func={ (event) => { this.setState({ point_id: event.target.value }) } } label='Точка' />
+          </Grid>
+
+          { !this.state.itemsEdit ? null :
+            <Dialog
+              open={this.state.modalDialog}
+              maxWidth={'md'}
+              onClose={ () => { this.setState({ modalDialog: false, itemsEdit: null, nameWork: '' }) } }
+            >
+              <DialogTitle>Уборка "{this.state.itemsEdit.item.name}"</DialogTitle>
+              <DialogContent>
+                
+                <Grid container spacing={0}>
+
+                  <Grid item xs={12}>
+                    <Typography>{this.state.itemsEdit.item.cat_name}</Typography>
+                  </Grid>
+
+                  { this.state.itemsEdit.item.show_time == false ? null :
+                    <Grid item xs={12}>
+                      <Typography>Время за уборку: {this.state.itemsEdit.item.time_min} мин.</Typography>
+                    </Grid>
+                  }
+
+                  <Grid item xs={12}>
+                    <Typography>Описание процесса уборки: </Typography>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Typography dangerouslySetInnerHTML={{__html: this.state.itemsEdit.item.text_work}}></Typography>
+                  </Grid>
+                  
+                </Grid>
+
+              </DialogContent>
+            </Dialog>
+          }
+
+          <Grid item xs={12} sm={12}>
+            <Paper>
+              <Tabs value={this.state.activeTab} onChange={ this.changeTab.bind(this) }>
+                <Tab label="Повар" {...a11yProps(0)} />
+                <Tab label="Кассир" {...a11yProps(1)} />
+                <Tab label="Кухонный работник" {...a11yProps(2)} />
+              </Tabs>
+            </Paper>
           </Grid>
 
           <Grid item xs={12} sm={12}>
@@ -251,7 +365,6 @@ class AppWorkShow_ extends React.Component {
               <TableHead>
                 <TableRow>
                   <TableCell>Работа</TableCell>
-                  <TableCell>Должность</TableCell>
                   <TableCell>День недели</TableCell>
                   <TableCell>Время открытия</TableCell>
                   <TableCell>Время автоматического удаления</TableCell>
@@ -260,10 +373,9 @@ class AppWorkShow_ extends React.Component {
 
               <TableBody>
                 
-                { this.state.items.map( (item, key) =>
+                { this.state.showItems.map( (item, key) =>
                   <TableRow key={key}>
                     <TableCell onClick={ this.openWork.bind(this, item.id) } style={{ color: '#c03', cursor: 'pointer', fontWeight: 'bold' }}>{item.work_name}</TableCell>
-                    <TableCell>{item.app_name}</TableCell>
                     <TableCell>{item.dow_name} {item.need_work_name}</TableCell>
                     <TableCell>{item.times_open}</TableCell>
                     <TableCell>{item.times_close}</TableCell>
