d81d8633bf9d80bd642322cef25b5e696c8b9387
Denis <adadenis1982@yandex.ru>
2022-10-26 16:10:56 +0300

    tender

--- src/components/tender/tender.component.jsx
+++ src/components/tender/tender.component.jsx
@@ -3,308 +3,354 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableContainer from '@mui/material/TableContainer';
 
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 
-import { MyAutocomplite } from '../../stores/elements';
+import { MyAutocomplite, MySelect } from '../../stores/elements';
 
 const queryString = require('query-string');
-
-
-const dynamicCellStyle = params => {
-  if( params.colDef.headerName == 'Цена' ){
-    return { borderRight: '1px solid #bababa' };
-  }
-  return null;
-};
 
 class Tender_ extends React.Component {
   constructor(props) {
     super(props);
-        
+
     this.state = {
       module: 'tender',
       module_name: '',
       is_load: false,
-      
-      modalDialog: false,
-      
-      testTable: [],
+
+      cities: [],
+      city: '',
+
+      towns: [
+        { id: '1', name: 'Все' },
+        { id: '2', name: 'ТЛТ' },
+        { id: '3', name: 'СМР' },
+      ],
+      town: '',
+
       vendors: [],
-      myVendors: [],
-      showVendors: [],
-      
-      params_api: null,
-      params_columnApi: null
+      vendor: '',
+
+      cats: []
+
     };
   }
-  
-  async componentDidMount(){
+
+  async componentDidMount() {
     let data = await this.getData('get_all');
-    
-    /*let table = [];
-    
-    data.items.map( (main_cat, k1) => {
-      
-      table.push({
-        section: 'big-title',
-        item: main_cat.name
-      })
-      
-      main_cat.cats.map( (cat, k2) => {
-        table.push({
-          section: 'big-title',
-          item: cat.name
-        })
-        
-        cat.items.map( (item, k3) => {
-          let tableItem = {};
-          
-          data['vendors'].map( (it, k4) => {
-            
-            let item_price = data.vendor_price.find( (item_price) => parseInt(item_price.vendor_id) == parseInt(it['id']) && parseInt(item_price.item_id) == parseInt(item['id']) );
-            
-            
-            if( item_price ){
-              tableItem[ 'vend_price_'+it['id'] ] = item_price['price'];  
-              tableItem[ 'vend_city_'+it['id'] ] = 'тлт';
-            }else{
-              tableItem[ 'vend_price_'+it['id'] ] = '';  
-              tableItem[ 'vend_city_'+it['id'] ] = '';
-            }
-            
-          } )
-          tableItem.item = item.name;
-          tableItem.section = 'last';
-          
-          let item_ras = data.ras.find( (item_ras) => parseInt(item_ras.item_id) == parseInt(item['id']) );
-          
-          if( item_ras ){
-            tableItem.ras = item_ras.count_pf;
-          }else{
-            tableItem.ras = '';
-          }
-          
-          table.push(tableItem)
-        } )
-      } )
-    } )*/
-    
+
+    console.log(data);
+
     this.setState({
       module_name: data.module_info.name,
-      //testTable: table,
-      //vendors: data['vendors'],
-      //showVendors: data['vendors']
-    })
-    
+      cities: data.cities,
+      city: data.cities[0].id,
+      vendors: data.vendors,
+    });
+
     document.title = data.module_info.name;
   }
-  
+
   getData = (method, data = {}) => {
-    
     this.setState({
-      is_load: true
-    })
-    
+      is_load: true,
+    });
+
     return fetch('https://jacochef.ru/api/index_new.php', {
       method: 'POST',
       headers: {
-        'Content-Type':'application/x-www-form-urlencoded'},
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
       body: queryString.stringify({
-        method: method, 
+        method: method,
         module: this.state.module,
         version: 2,
         login: localStorage.getItem('token'),
-        data: JSON.stringify( data )
+        data: JSON.stringify(data),
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.st === false && json.type == 'redir') {
+          window.location.pathname = '/';
+          return;
+        }
+
+        if (json.st === false && json.type == 'auth') {
+          window.location.pathname = '/auth';
+          return;
+        }
+
+        setTimeout(() => {
+          this.setState({
+            is_load: false,
+          });
+        }, 300);
+
+        return json;
       })
-    }).then(res => res.json()).then(json => {
-      
-      if( json.st === false && json.type == 'redir' ){
-        window.location.pathname = '/';
-        return;
-      }
-      
-      if( json.st === false && json.type == 'auth' ){
-        window.location.pathname = '/auth';
-        return;
-      }
-      
-      setTimeout( () => {
+      .catch((err) => {
+        console.log(err);
         this.setState({
-          is_load: false
-        })
-      }, 300 )
-      
-      return json;
-    })
-    .catch(err => { 
-      console.log( err )
-      this.setState({
-        is_load: false
-      })
-    });
-  }
-  
-  onCellClicked(event){
-    console.log( event.data );
-    console.log( event.colDef.field );
-  }
-  
-  onGridReady(params){
+          is_load: false,
+        });
+      });
+  };
+
+  changeCity(event) {
     this.setState({
-      params_api: params.api,
-      params_columnApi: params.columnApi
+      city: event.target.value,
     });
   }
-  
-  async show(){
-    let data = {
-      vendors: this.state.myVendors
+
+  changeVendor(event, value) {
+    this.setState({
+      vendor: value,
+    });
+  }
+
+  async getDataTable() {
+    const data = {
+      city_id: this.state.city,
     };
-    
+
     let res = await this.getData('get_data', data);
-    
-    let table = [];
-    
-    res.items.map( (main_cat, k1) => {
-      
-      table.push({
-        section: 'big-title',
-        item: main_cat.name
-      })
-      
-      main_cat.cats.map( (cat, k2) => {
-        table.push({
-          section: 'big-title',
-          item: cat.name
-        })
-        
-        cat.items.map( (item, k3) => {
-          let tableItem = {};
-          
-          res['vendors'].map( (it, k4) => {
-            
-            let item_price = res.vendor_price.find( (item_price) => parseInt(item_price.vendor_id) == parseInt(it['id']) && parseInt(item_price.item_id) == parseInt(item['id']) )
-            
-            if( item_price ){
-              tableItem[ 'vend_price_'+it['id'] ] = item_price['price'];  
-              tableItem[ 'vend_city_'+it['id'] ] = 'тлт';
-            }else{
-              tableItem[ 'vend_price_'+it['id'] ] = '';  
-              tableItem[ 'vend_city_'+it['id'] ] = '';
-            }
-            
-          } )
-          tableItem.item = item.name;
-          tableItem.section = 'last';
-          
-          let item_ras = res.ras.find( (item_ras) => parseInt(item_ras.item_id) == parseInt(item['id']) );
-          
-          if( item_ras ){
-            tableItem.ras = item_ras.count_pf;
-          }else{
-            tableItem.ras = '';
-          }
-          
-          table.push(tableItem)
-        } )
-      } )
-    } )
-    
-    
-    
-    
-    
-    
+
+    console.log(res);
+
+    const vendors = this.state.vendors;
+
+    const items = res.vendor_items_price;
+
+    const vendors_items = vendors.map((el) => {
+      el.price = [];
+
+      items.forEach((it) => {
+        if (it.vendor_id === el.id) {
+          el.price.push(it);
+        }
+      });
+
+      return el;
+    });
+
     this.setState({
-      testTable: table,
-      showVendors: res['vendors']
-    })
+      cats: res.items,
+      vendors: vendors_items,
+    });
   }
-  
-  render(){
 
-    return (
-      <Backdrop open={true}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
-    )
+  changeSelect(event) {
+    this.setState({
+      town: event.target.value,
+    });
 
+  }
+
+  render() {
     return (
       <>
         <Backdrop open={this.state.is_load}>
           <CircularProgress color="inherit" />
         </Backdrop>
-        
-        <Dialog
-          open={this.state.modalDialog}
-          onClose={ () => { this.setState({ modalDialog: false }) } }
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">Новая категория</DialogTitle>
-          <DialogContent>
-            
-            
-            
-          </DialogContent>
-          <DialogActions>
-            <Button  color="primary">Сохранить</Button>
-          </DialogActions>
-        </Dialog>
-        
-        <Grid container spacing={3}>
+
+        <Grid container spacing={3} mb={3}>
           <Grid item xs={12} sm={12}>
             <h1>{this.state.module_name}</h1>
           </Grid>
-          
-          <Grid item xs={12} sm={12}>
-            <MyAutocomplite data={this.state.vendors} multiple={true} value={this.state.myVendors} func={ (event, val) => { this.setState({ myVendors: val }) } } label='Поставщики' />
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Button variant="contained" onClick={this.show.bind(this)}>Показать</Button>
-          </Grid>
-          
-          <Grid item xs={12}>
-            <div className="ag-theme-alpine" style={{height: 600, width: '100%'}}>
-              <AgGridReact
-                  onGridReady={this.onGridReady.bind(this)}
-                  onCellClicked={this.onCellClicked}
-                  rowData={ this.state.testTable }
-                  
-                  >
-                  <AgGridColumn field="ras" width={120} pinned="left"></AgGridColumn>
-                  <AgGridColumn field="item" width={300} pinned="left" cellClassRules={{'cell-span': "data.section==='big-title'"}}></AgGridColumn>
-                  <AgGridColumn hide={true}></AgGridColumn>
-                    
-                  { this.state.showVendors.map( (item, key) => 
-                    <AgGridColumn key={key} headerName={item.name} resizable={true} width={180}>
-                      <AgGridColumn field={'vend_city_'+item.id} headerName={'Город'} resizable={true} width={90}></AgGridColumn>
-                      <AgGridColumn field={'vend_price_'+item.id} headerName={'Цена'} resizable={true} width={90} cellStyle={dynamicCellStyle}></AgGridColumn>
-                    </AgGridColumn>
-                  ) }
-                  
-                  
-              </AgGridReact>
-            </div>
-          </Grid>
-          
-          <Grid item xs={12} sm={12}>
-          
-          </Grid>    
         </Grid>
+
+        <Grid container spacing={3} justifyContent="center" mb={5}>
+          <Grid item xs={12} sm={3}>
+            <MySelect
+              data={this.state.cities}
+              value={this.state.city}
+              func={this.changeCity.bind(this)}
+              label="Город"
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={3}>
+            <MyAutocomplite
+              label="Поставщик"
+              multiple={false}
+              data={this.state.vendors}
+              value={this.state.vendor}
+              func={this.changeVendor.bind(this)}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={3}>
+            <Button
+              variant="contained"
+              style={{ whiteSpace: 'nowrap' }}
+              onClick={this.getDataTable.bind(this)}
+            >
+              Обновить данные
+            </Button>
+          </Grid>
+        </Grid>
+
+        {!this.state.cats.length ? null : (
+          <Grid container spacing={3} >
+            <Grid item xs={12} sm={12}>
+              <TableContainer sx={{ maxHeight: { xs: 'none', sm: 700 } }}>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell sx={{ backgroundColor: '#d87093' }}>
+                        Категория
+                      </TableCell>
+                      <TableCell sx={{ backgroundColor: '#d87093' }}>
+                        Параметры
+                      </TableCell>
+                      <TableCell sx={{ backgroundColor: '#d87093' }}>
+                        Все
+                      </TableCell>
+                      <TableCell sx={{ backgroundColor: '#d87093' }}>
+                        Тольятти
+                      </TableCell>
+                      <TableCell sx={{ backgroundColor: '#d87093' }}>
+                        Самара
+                      </TableCell>
+                      {this.state.vendors.map((vendor, key) => (
+                        <React.Fragment key={key}>
+                          <TableCell
+                            style={{
+                              whiteSpace: 'nowrap',
+                              backgroundColor: '#d87093',
+                            }}
+                            colSpan={2}
+                          >
+                            {vendor.name}
+                          </TableCell>
+                        </React.Fragment>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+
+                  <TableBody>
+                    {this.state.cats.map((item, key) => (
+                      <React.Fragment key={key}>
+                        <TableRow >
+                          <TableCell
+                            colSpan={`${5 + this.state.vendors.length * 2}`}
+                            sx={{
+                              backgroundColor: '#98FB98',
+                            }}
+                          >
+                            {item.name}
+                          </TableCell>
+                        </TableRow>
+
+                        {item.cats.map((category, key_cat) => (
+                          <React.Fragment key={key_cat}>
+                            <TableRow
+                              sx={{ '& td': { backgroundColor: '#40E0D0' } }}
+                            >
+                              <TableCell>{category.name}</TableCell>
+                              <TableCell></TableCell>
+                              {key_cat === 0 ? (
+                                <TableCell colSpan={3}>
+                                  Зафиксированные цены
+                                </TableCell>
+                              ) : (
+                                <React.Fragment key={key_cat}>
+                                  <TableCell></TableCell>
+                                  <TableCell></TableCell>
+                                  <TableCell></TableCell>
+                                </React.Fragment>
+                              )}
+                              {this.state.vendors.map((vendor, key) => (
+                                <React.Fragment key={key}>
+                                  <TableCell colSpan={2}></TableCell>
+                                </React.Fragment>
+                              ))}
+                            </TableRow>
+
+                            {category.items.map((item, k) => (
+                              <TableRow key={k} hover>
+                                <TableCell className="td_color">
+                                  {item.name}
+                                </TableCell>
+                                <TableCell></TableCell>
+                                <TableCell></TableCell>
+                                <TableCell></TableCell>
+                                <TableCell></TableCell>
+                                {this.state.vendors.map((vendor, key) => (
+                                  <React.Fragment key={key}>
+                                    {vendor.price.find(
+                                      (it) => it.item_id === item.id
+                                    ) ? (
+                                      <>
+                                        <TableCell 
+                                        style={{ backgroundColor: 
+                                        this.state.town === '1' ? '#FFFF00' :
+                                        this.state.town === '2' ? '#FF4500' :
+                                        this.state.town === '3' ? '#8B008B' :
+                                        'none'
+                                      }}>
+                                          <MySelect
+                                            data={this.state.towns}
+                                            value={this.state.town}
+                                            func={this.changeSelect.bind(this)}
+                                            label=""
+                                          />
+                                        </TableCell>
+                                        <TableCell
+                                          style={{
+                                            whiteSpace: 'nowrap',
+                                            backgroundColor: '#00BFFF',
+                                            textAlign: 'center',
+                                          }}
+                                        >
+                                          {`${Number(
+                                            vendor.price.find(
+                                              (it) => it.item_id === item.id
+                                            ).price
+                                          ).toFixed(2)} P`}
+                                        </TableCell>
+                                      </>
+                                    ) : (
+                                      <>
+                                        <TableCell></TableCell>
+                                        <TableCell
+                                          style={{
+                                            whiteSpace: 'nowrap',
+                                            color: 'rgba(0, 0, 0, 0.5)',
+                                          }}
+                                        >
+                                          нет
+                                        </TableCell>
+                                      </>
+                                    )}
+                                  </React.Fragment>
+                                ))}
+                              </TableRow>
+                            ))}
+                          </React.Fragment>
+                        ))}
+                      </React.Fragment>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+          </Grid>
+        )}
       </>
-    )
+    );
   }
 }
 
 export function Tender() {
-  return (
-    <Tender_ />
-  );
+  return <Tender_ />;
 }
