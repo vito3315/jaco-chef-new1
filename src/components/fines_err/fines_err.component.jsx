1900c228f22e8acde43a8c8643675e3fbc6b37f0
vito3315 <marusiaqwerti@yandex.ru>
2022-12-15 21:49:40 +0400

    fines_err

--- src/components/fines_err/fines_err.component.jsx
+++ src/components/fines_err/fines_err.component.jsx
@@ -55,6 +55,7 @@ class Fines_err_Modal_item extends React.Component {
 
     this.state = {
       item: null,
+      text: '',
 
       users: [],
       user: '',
@@ -93,40 +94,40 @@ class Fines_err_Modal_item extends React.Component {
 
     if (value) {
       
-      const app = apps.find(app => app.id === (value.new_app === '0' ? value.app_id : value.new_app));
+      //const app = apps.find(app => app.id === (value.new_app === '0' ? value.app_id : value.new_app));
 
       this.setState({
         user: value,
         selected: value.id,
-        app,
-        app_for_search: app.id,
+        //app,
+        //app_for_search: app.id,
       });
     } else {
       this.setState({
         user: value,
         selected: '',
-        app: apps[0],
-        app_for_search: '',
+        //app: apps[0],
+        //app_for_search: '',
       });
     }
   }
 
   changePost(data, event, value) {
 
-    const user = this.state.user;
+    //const user = this.state.user;
     
-    if(user && value.id !== -1) {
+    /*if(user && value.id !== -1) {
       this.setState({
         [data]: value,
       });
-    } else {
+    } else {*/
       this.setState({
         [data]: value,
         app_for_search: value.id === -1 ? '' : value.id,
         selected: '',
         user: '',
       });
-    }
+    //}
 
   }
 
@@ -138,15 +139,15 @@ class Fines_err_Modal_item extends React.Component {
     if (event.target.classList.contains('choose_user')) {
       this.setState({
         user: '',
-        app: apps[0],
-        app_for_search: '',
+        //app: apps[0],
+        //app_for_search: '',
         selected: '',
       });
     } else {
       this.setState({
         user,
-        app,
-        app_for_search: app.id,
+        //app,
+        //app_for_search: app.id,
         selected: user.id,
       });
     }
@@ -171,6 +172,7 @@ class Fines_err_Modal_item extends React.Component {
     const data = {
       id: item.err.id,
       user: user.id,
+      text: this.state.text
     }
 
     this.props.save(data);
@@ -190,6 +192,17 @@ class Fines_err_Modal_item extends React.Component {
     });
 
     this.props.onClose();
+  }
+
+  saveErr(type){
+    const data = {
+      id: item.err.id,
+      type: type
+    }
+
+    if (confirm( (type === 'true' ? 'Подтвердить' : 'Удалить' ) + ' ошибку ?')) {
+      this.props.saveErr(data);
+    } 
   }
 
   render() {
@@ -253,6 +266,48 @@ class Fines_err_Modal_item extends React.Component {
                   {this.state.item ? this.state.item.err.fine_name : 'не указана'}
                 </Typography>
               </Grid>
+
+              { this.state.item && parseInt(this.state.item.err.manager_create) == 1 && parseInt(this.state.item.err.status) == 2 && parseInt(this.state.item.err.for_dir) == 1 ?
+                <>
+                  <Grid item xs={12} sm={6}>
+                    <Button color="primary" onClick={this.saveErr.bind(this, 'true')}>Подтвердить ошибку</Button>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <Button color="primary" onClick={this.saveErr.bind(this, 'false')}>Отменить ошибку</Button>
+                  </Grid>
+                </>
+                  :
+                null
+              }
+
+              { this.state.item && this.state.item.err.text_one.length > 0 ?
+                <Grid item xs={12} sm={6} display="flex" flexDirection="column" alignItems="center">
+                  <Typography sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}>Причина обжалования</Typography>
+                  <Typography sx={{ fontWeight: 'normal', textAlign: 'center' }}>{this.state.item.err.text_one}</Typography>
+                </Grid>
+                  :
+                null
+              }
+
+              { this.state.item && this.state.item.err.text_one.length > 0 ?
+                this.state.item.err.text_two.length > 0 ?
+                  <Grid item xs={12} sm={6}>
+                    <Typography sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}>Описание решения</Typography>
+                    <Typography sx={{ fontWeight: 'normal', textAlign: 'center' }}>{this.state.item.err.text_one}</Typography>
+                  </Grid>
+                    :
+                  <Grid item xs={12} sm={6}>
+                    <MyTextInput 
+                      label="Описание решения" 
+                      value={this.state.text} 
+                      func={ (event) => { this.setState({ text: event.target.value }) } }
+                      multiline={true}
+                      maxRows={7}
+                    />
+                  </Grid>
+                  :
+                null
+              }
 
               <Grid item xs={12} sm={6} display="flex" flexDirection="column" alignItems="center">
                 <Typography sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}>Фото ошибки</Typography>
@@ -404,20 +459,20 @@ class Fines_err_Modal_NewItem extends React.Component {
 
     if (value) {
       
-      const app = apps.find(app => app.id === (value.new_app === '0' ? value.app_id : value.new_app));
+      //const app = apps.find(app => app.id === (value.new_app === '0' ? value.app_id : value.new_app));
 
       this.setState({
         user: value,
         selected: value.id,
-        app,
-        app_for_search: app.id,
+        //app,
+        //app_for_search: app.id,
       });
     } else {
       this.setState({
         user: value,
         selected: '',
-        app: apps[0],
-        app_for_search: '',
+        //app: apps[0],
+        //app_for_search: '',
       });
     }
   }
@@ -444,20 +499,20 @@ class Fines_err_Modal_NewItem extends React.Component {
 
   changePost(data, event, value) {
 
-    const user = this.state.user;
+    //const user = this.state.user;
     
-    if(user && value.id !== -1) {
+    /*if(user && value.id !== -1) {
       this.setState({
         [data]: value,
       });
-    } else {
+    } else {*/
       this.setState({
         [data]: value,
         app_for_search: value.id === -1 ? '' : value.id,
         selected: '',
         user: '',
       });
-    }
+    //}
 
   }
 
@@ -530,15 +585,15 @@ class Fines_err_Modal_NewItem extends React.Component {
     if (event.target.classList.contains('choose_user')) {
       this.setState({
         user: '',
-        app: apps[0],
-        app_for_search: '',
+        //app: apps[0],
+        //app_for_search: '',
         selected: '',
       });
     } else {
       this.setState({
         user,
-        app,
-        app_for_search: app.id,
+        //app,
+        //app_for_search: app.id,
         selected: user.id,
       });
     }
@@ -1078,8 +1133,6 @@ class Fines_err_ extends React.Component {
   }
 
   async saveEditItem(data) {
-    // console.log(data);
-
     let res = await this.getData('update', data);
 
     if(res.st) {
@@ -1108,6 +1161,27 @@ class Fines_err_ extends React.Component {
     // await this.getData('save_new', data);
 
     // this.getItems();
+  }
+
+  async saveErr(data){
+    let res = await this.getData('saveErr', data);
+
+    if(res.st) {
+      this.setState({ modalDialog: false });
+
+      setTimeout( () => {
+        this.getItems();
+      }, 300 )
+
+    } else {
+
+      this.setState({
+        openAlert: true,
+        err_status: res.st,
+        err_text: res.text,
+      });
+
+    }
   }
 
   fullCloseDialog(){
