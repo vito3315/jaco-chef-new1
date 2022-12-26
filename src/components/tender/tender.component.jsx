3bd323b88d2b24bda347651def64b9d849172d67
chyvi <74423767+chyvi@users.noreply.github.com>
2022-12-26 15:46:59 +0400

    tender
    Правки по Excel

--- src/components/tender/tender.component.jsx
+++ src/components/tender/tender.component.jsx
@@ -298,10 +298,13 @@ class Tender_ extends React.Component {
     };
 
     const res =  await this.getData('downLoadVendor', data);
-    
-    this.setState({
-      url: res.url
-    });
+
+    // правка 26.12 скачивания файла в один клик
+    if( res.url){
+      const link = document.createElement('a');
+      link.href = res.url;
+      link.click();
+    }
   }
 
   onDownload() {
@@ -464,27 +467,7 @@ class Tender_ extends React.Component {
                           }}
                           onClick={this.downLoadVendor.bind(this, vendor.id)}
                         >
-                          подготовить
-                        </TableCell>
-                       
-                      ))}
-                    </TableRow>
-
-                    <TableRow>
-                      <TableCell style={{ zIndex: 3 }}></TableCell>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                      {this.state.vendors.map((vendor, key) => (
-                        <TableCell
-                          key={key}
-                          style={{
-                            maxWidth: 150,
-                            textAlign: 'center',
-                            cursor: 'pointer'
-                          }}
-                          onClick={this.onDownload.bind(this, vendor.id)}
-                        >
-                          скачать
+                          Скачать
                         </TableCell>
                        
                       ))}
