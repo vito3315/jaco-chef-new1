4ff2bb48b4e77008f3edb347c4fca415fea022a0
vito3315 <marusiaqwerti@yandex.ru>
2023-04-23 11:43:22 +0400

    site_items

--- src/components/site_items/site_items.component.jsx
+++ src/components/site_items/site_items.component.jsx
@@ -1655,20 +1655,10 @@ class SiteItems_ extends React.Component {
 
                                   <Grid item xs={12} sm={6}>
                                     { this.state.editItem.img_app.length > 0 ?
-                                      <picture>
-                                        <source srcSet={`
-                                            https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_276x276.jpg 138w, 
-                                            https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_292x292.jpg 146w,
-                                            https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_366x366.jpg 183w,
-                                            https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_466x466.jpg 233w,
-                                            https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_585x585.jpg 292w
-                                            https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_732x732.jpg 366w,
-                                            https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_1168x1168.jpg 584w,
-                                            https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_1420x1420.jpg 760w,
-                                            https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_2000x2000.jpg 1875w`} 
-                                            sizes="(max-width=1439px) 233px, (max-width=1279px) 218px, 292px" />
-                                        <img style={{ maxHeight: 300 }} src={`https://storage.yandexcloud.net/site-img/${this.state.editItem.img_app}_276х276.jpg`} />
-                                      </picture>
+                                      <img 
+                                        src={"https://cdnimg.jacofood.ru/"+this.state.editItem.img_app+"_732x732.jpg"} 
+                                        style={{ width: '100%', height: 'auto' }}
+                                      />
                                         :
                                       <div style={{maxWidth: 300, maxHeight: 300}}/>
                                     }
